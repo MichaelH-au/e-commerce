75aaec3167dca0b03464c36a5d06fd20dcabbbd8
Michael <huangjkau@gmail.com>
2019-01-18 16:28:21 +1100

    fixed the bug in home page

--- ecommerce/src/components/ItemArea/ItemArea.js
+++ ecommerce/src/components/ItemArea/ItemArea.js
@@ -13,7 +13,6 @@ class ItemArea extends Component {
         $('#addToCart').modal('show')
     }
     render() {
-        console.log(this.props.user)
         return (
             <div>
                 <div className='itemArea pb-5'>
