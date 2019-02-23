8f8b296e6b56319ba44221eb826afe2e2088a5be
Michael <huangjkau@gmail.com>
2019-02-23 12:49:50 +1100

    create order detail page

--- ecommerce/src/containers/MyAccount/subpages/orderDetail/index.js
+++ ecommerce/src/containers/MyAccount/subpages/orderDetail/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ConfirmList from '../../../../components/confirmList/index'
 
 class Index extends Component {
     render() {
@@ -7,9 +8,23 @@ class Index extends Component {
         return (
             <div>
                 <div className='greyColor fontSizeLarge font-weight-bold mb-3 mt-3 p-0'>Order Detail </div>
-                <div className='row'>
+                <div className='row mb-2'>
                     <div className='col'>Order Number: {order.orderId}</div>
                     <div className='col'>Create Time: {order.created_at.substring(0,19)}</div>
+                </div>
+                <div className='row mb-2'>
+                    <div className='col'>Receiver: {order.address.contactName}</div>
+                    <div className='col'>Address: {order.address.address}</div>
+                </div>
+                <div className='mb-2'>Order Status: {order.status}</div>
+                <div className='container text-center bg-white mt-3 pb-5'>
+                    <div className='row text-center fontSizeSmall w-100 cartTitle p-2'>
+                        <div className="col-6">Items</div>
+                        <div className="col-2">Price</div>
+                        <div className="col-2">Quantity</div>
+                        <div className="col-2">Subtotal</div>
+                    </div>
+                    <ConfirmList data={order.orderInfo}/>
                 </div>
             </div>
         );
